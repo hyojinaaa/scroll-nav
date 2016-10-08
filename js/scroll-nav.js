74e4aec25eec4bfa53d5e0b0d3e591c3a4867c6b
hyojinaaa <hyojinjin@hotmail.com>
2016-10-08 18:22:13 +1300

    Add jquery function

--- js/scroll-nav.js
+++ js/scroll-nav.js
@@ -1,1 +1,16 @@
-// 
+$(document).ready(function(){
+
+	$(window).scroll(function(){
+
+		var scrollPosition = $(window).scrollTop();
+		var nav = $('#nav');
+
+		if( scrollPosition > 40 ) {
+			 nav.css('background-color', 'orange');
+		} else {
+			nav.css('background-color', 'transparent');
+		}
+
+	});
+
+});
